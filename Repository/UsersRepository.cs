f7b672c1b8b44d56f9bfb3f0fa6c034837d17689
Emanuel <lejiei@gmail.com>
2020-09-24 22:23:30 -0300

    d

--- Repository/UsersRepository.cs
+++ Repository/UsersRepository.cs
@@ -12,7 +12,7 @@ namespace Repository
     {
         string conn = "Data Source=DESKTOP-S1UKRBH\\SQLEXPRESS;Initial Catalog=RAM;Integrated Security=True";
 
-        //555dssdffddddsssdddddjjjfddddddddsssss
+        //555dssdffddddsssdddddjjjfddddddddssssss
         public List<Users> GetUsuarios()
         {
             List<Users> lstusers = new List<Users>();
